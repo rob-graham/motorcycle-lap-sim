8d2e5e3e8c24db8e849b36c214de220633a1da84
rob-graham <info@graham.id.au>
2026-08-16 16:16:52 +0930

    Phase 10: export peer trajectory consistency metric

--- src/motorcycle_lap_sim/telemetry/__init__.py
+++ src/motorcycle_lap_sim/telemetry/__init__.py
@@ -3,6 +3,7 @@
 from .aim_excel import load_aim_workbook
 from .aim_quality import load_aim_gps_quality
 from .model import TelemetryLap, TelemetrySession, lap_slices
+from .peer import PeerTrajectoryDeviation, peer_trajectory_deviation
 from .quality import GPSQualitySeries, gps_quality_mask, require_time_alignment
 from .registration import RigidRegistrationResult, fit_rigid_registration
 from .repeatability import (
@@ -24,6 +25,8 @@ __all__ = [
     "require_time_alignment",
     "RigidRegistrationResult",
     "fit_rigid_registration",
+    "PeerTrajectoryDeviation",
+    "peer_trajectory_deviation",
     "ChainageProgressDiagnostics",
     "CrossLapEnvelope",
     "unwrap_closed_chainage",
