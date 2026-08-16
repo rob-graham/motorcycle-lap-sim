398907ea5d33f055f53316a54495efd77afd4433
rob-graham <info@graham.id.au>
2026-08-16 16:10:19 +0930

    Phase 10: export repeatability diagnostics

--- src/motorcycle_lap_sim/telemetry/__init__.py
+++ src/motorcycle_lap_sim/telemetry/__init__.py
@@ -5,6 +5,13 @@ from .aim_quality import load_aim_gps_quality
 from .model import TelemetryLap, TelemetrySession, lap_slices
 from .quality import GPSQualitySeries, gps_quality_mask, require_time_alignment
 from .registration import RigidRegistrationResult, fit_rigid_registration
+from .repeatability import (
+    ChainageProgressDiagnostics,
+    CrossLapEnvelope,
+    chainage_progress_diagnostics,
+    cross_lap_envelope,
+    unwrap_closed_chainage,
+)
 
 __all__ = [
     "TelemetryLap",
@@ -17,4 +24,9 @@ __all__ = [
     "require_time_alignment",
     "RigidRegistrationResult",
     "fit_rigid_registration",
+    "ChainageProgressDiagnostics",
+    "CrossLapEnvelope",
+    "unwrap_closed_chainage",
+    "chainage_progress_diagnostics",
+    "cross_lap_envelope",
 ]
