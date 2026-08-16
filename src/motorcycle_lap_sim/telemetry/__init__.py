ab6c179c96aa76527d43652cc92b13af11cf1e1c
rob-graham <info@graham.id.au>
2026-08-16 16:03:44 +0930

    Phase 10: expose GPS quality helpers

--- src/motorcycle_lap_sim/telemetry/__init__.py
+++ src/motorcycle_lap_sim/telemetry/__init__.py
@@ -1,7 +1,9 @@
 """Measured telemetry import and two-dimensional validation helpers."""
 
 from .aim_excel import load_aim_workbook
+from .aim_quality import load_aim_gps_quality
 from .model import TelemetryLap, TelemetrySession, lap_slices
+from .quality import GPSQualitySeries, gps_quality_mask, require_time_alignment
 from .registration import RigidRegistrationResult, fit_rigid_registration
 
 __all__ = [
@@ -9,6 +11,10 @@ __all__ = [
     "TelemetrySession",
     "lap_slices",
     "load_aim_workbook",
+    "GPSQualitySeries",
+    "load_aim_gps_quality",
+    "gps_quality_mask",
+    "require_time_alignment",
     "RigidRegistrationResult",
     "fit_rigid_registration",
 ]
