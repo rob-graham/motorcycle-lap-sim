efc346137259187a9158d855f6632bb147bf26fc
rob-graham <info@graham.id.au>
2026-08-16 15:56:16 +0930

    Phase 10: export rigid registration helpers

--- src/motorcycle_lap_sim/telemetry/__init__.py
+++ src/motorcycle_lap_sim/telemetry/__init__.py
@@ -2,5 +2,13 @@
 
 from .aim_excel import load_aim_workbook
 from .model import TelemetryLap, TelemetrySession, lap_slices
+from .registration import RigidRegistrationResult, fit_rigid_registration
 
-__all__ = ["TelemetryLap", "TelemetrySession", "lap_slices", "load_aim_workbook"]
+__all__ = [
+    "TelemetryLap",
+    "TelemetrySession",
+    "lap_slices",
+    "load_aim_workbook",
+    "RigidRegistrationResult",
+    "fit_rigid_registration",
+]
