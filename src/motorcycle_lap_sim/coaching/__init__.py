f15643c6e798dfe27d078d4fc7ce104fc07c4ab4
rob-graham <info@graham.id.au>
2026-08-17 23:42:03 +0930

    Expose coaching corner-region detector

--- src/motorcycle_lap_sim/coaching/__init__.py
+++ src/motorcycle_lap_sim/coaching/__init__.py
@@ -1,5 +1,15 @@
 """Rider-facing coaching-event extraction."""
 
-from .events import CoachingEvent, EventDetectionConfig, extract_coaching_events
+from .events import (
+    CoachingEvent,
+    EventDetectionConfig,
+    detect_corner_regions,
+    extract_coaching_events,
+)
 
-__all__ = ["CoachingEvent", "EventDetectionConfig", "extract_coaching_events"]
+__all__ = [
+    "CoachingEvent",
+    "EventDetectionConfig",
+    "detect_corner_regions",
+    "extract_coaching_events",
+]
